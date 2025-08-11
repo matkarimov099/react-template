2328513dfc7f7e05d2070ba7878e58709cf80518
Matkarim <matkarimov1099@gmail.com>
2025-08-11 10:07:36 +0500

    some changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'node:path';
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),  tailwindcss(),],
+  plugins: [react(), tailwindcss(),],
   build: {
     rollupOptions: {
       output: {
